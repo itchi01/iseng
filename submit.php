4f2712ac824ba521d0e3d6fc1398a3a83ad3588b
ferza nanda <ferzananda97@gmail.com>
2020-03-20 01:28:35 -0700

    aplha

---
+++ submit.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+</body>
+	Terimakasih Akun Email :
+	<?php echo _$POST["email"]; ?> <br>
+	dan Password :
+	<?php echo _$POST['password']; ?><br>
+	anda telah siap dibuat, silahkan <a href="#">Clik disini </a> untuk kembali ke menu utama.
+</body>
+</html>
